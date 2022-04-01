fa7327f3937b29f07e5e56840a802c01d7999556
Canepa225 <canepa.igna@gmail.com>
2022-04-01 10:07:03 -0300

    Desafío triangulo subido

--- 01/pintame.py
+++ 01/pintame.py
@@ -1,9 +1,15 @@
 def pintame():
+
     ancho = int(input("Ingrese ancho: "))
     alto = int(input("Ingrese alto: "))
     caracter = input("Ingrese caracter: ")
 
-    for x in range(0, alto):
-        print(caracter*ancho)
+    option = input("¿Qué quieres pintar? Rectangulo(1) o Triangulo(2): ")
+    if option == "1":
+        for x in range(0, alto):
+            print(caracter*ancho)
+    elif option == "2":
+        for fila in range(0, alto):
+            print(" " * (alto-fila) + caracter * (2*fila - 1))
 
 pintame()
